1e830858c2d330af8e04daf094ec243f16007dcc
Bradley M. Small <bradley_small@hotmail.com>
2019-10-31 20:46:01 -0400

    making lint corrections

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-# This problem was asked by Jane Street.
-#
-# cons(a, b) constructs a pair,
-# and car(pair) and cdr(pair) returns the first and last element of that
-# pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4))
-# returns 4.
-#
-# Given this implementation of cons:
-#
-# def cons(a, b):
-#     def pair(f):
-#         return f(a, b)
-#     return pair
-#
-# Implement car and cdr
+#!/usr/bin/python
+"""This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair,
+and car(pair) and cdr(pair) returns the first and last element of that
+pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4))
+returns 4.
+
+Given this implementation of cons:
 
 def cons(a, b):
     def pair(f):
         return f(a, b)
     return pair
 
+Implement car and cdr
+"""
+
+
+def cons(lhs, rhs):
+    """Demonstrate cons with closure."""
+    def pair(func):
+        return func(lhs, rhs)
+    return pair
+
 
 def car(pair):
-    def f(a, b):
-        return a
-    return pair(f)
+    """Demonstrate car with closure."""
+    def func(lhs, _):
+        return lhs
+    return pair(func)
 
 
 def cdr(pair):
-    def f(a, b):
-        return b
-    return pair(f)
+    """Demonstrate cdr with closure."""
+    def func(_, rhs):
+        return rhs
+    return pair(func)
 
 
-print cdr(cons(3,4))
-print car(cons(3,4))
+if __name__ == "__main__":
+    print(cdr(cons(3, 4)))
+    print(car(cons(3, 4)))
